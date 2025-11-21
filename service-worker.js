4b373d0cc9a4ea7dd9f58652a494f258caa705b6
Rémi BIOU <remi.biou@canal-plus.com>
2025-11-21 19:25:52 +0100

    Add initial project files, including icons, manifest, and service worker

---
+++ service-worker.js
@@ -0,0 +1,57 @@
+const CACHE_NAME = 'quicuit-cache-v1';
+const CACHE_ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './service-worker.js',
+  './icons/icon-192.png',
+  './icons/icon-512.png',
+  './assets/'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(CACHE_ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') {
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+
+      return fetch(event.request)
+        .then((networkResponse) => {
+          if (!networkResponse || networkResponse.status !== 200 || networkResponse.type !== 'basic') {
+            return networkResponse;
+          }
+
+          const responseClone = networkResponse.clone();
+          caches.open(CACHE_NAME).then((cache) => {
+            cache.put(event.request, responseClone);
+          });
+
+          return networkResponse;
+        })
+        .catch(() => caches.match('./index.html'));
+    })
+  );
+});
